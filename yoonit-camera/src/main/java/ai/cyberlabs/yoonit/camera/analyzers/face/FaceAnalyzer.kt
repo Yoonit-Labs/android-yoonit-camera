22efa0f5f83b47cc7d522f9153199e9fe353f19b
HaroldoTeruya <haroldo.s.teruya@gmail.com>
2020-11-26 13:49:11 -0300

    Refactor face analyzer detection box validations

--- yoonit-camera/src/main/java/ai/cyberlabs/yoonit/camera/analyzers/face/FaceAnalyzer.kt
+++ yoonit-camera/src/main/java/ai/cyberlabs/yoonit/camera/analyzers/face/FaceAnalyzer.kt
@@ -46,7 +46,7 @@ class FaceAnalyzer(
 ) : ImageAnalysis.Analyzer {
 
     private var analyzerTimeStamp: Long = 0
-    private var hasStatus: Boolean = false
+    private var isValid: Boolean = false
     private var numberOfImages = 0
 
     private val faceBoundingBoxController = FaceBoundingBoxController(
@@ -93,23 +93,23 @@ class FaceAnalyzer(
                 )
 
                 // Get status if exist.
-                val status = this.getStatus(closestFace, detectionBox)
+                val error = this.getError(closestFace, detectionBox)
 
                 // Emit once if has error.
-                if (status != null) {
-                    if (this.hasStatus) {
-                        this.hasStatus = false
+                if (error != null) {
+                    if (this.isValid) {
+                        this.isValid = false
                         this.graphicView.clear()
                         if (this.cameraEventListener != null) {
-                            if (status != "") {
-                                this.cameraEventListener.onMessage(status)
+                            if (error != "") {
+                                this.cameraEventListener.onMessage(error)
                             }
                             this.cameraEventListener.onFaceUndetected()
                         }
                     }
                     return@addOnSuccessListener
                 }
-                this.hasStatus = true
+                this.isValid = true
 
                 // Draw or clean the bounding box based on the "faceDetectionBox".
                 if (this.captureOptions.faceDetectionBox) {
@@ -161,38 +161,79 @@ class FaceAnalyzer(
     }
 
     /**
-     *  Get the status if exist in the parameters.
+     *  Get the error message if exist based in the capture options rules,
+     *  closest face and detection box.
      *
      *  @param closestFace The closest face detected.
      *  @param detectionBox The closest face detected bounding box normalized coordinates.
      *
-     *  @return "" for status undefined, string or null if no status found.
+     *  @return null for no error:
+     *  - INVALID_CAPTURE_FACE_MIN_SIZE
+     *  - INVALID_CAPTURE_FACE_MAX_SIZE
+     *  - INVALID_CAPTURE_FACE_OUT_OF_ROI
+     *  - INVALID_CAPTURE_FACE_ROI_MIN_SIZE
      */
-    private fun getStatus(closestFace: Face?, detectionBox: RectF?): String? {
+    private fun getError(closestFace: Face?, detectionBox: RectF?): String? {
+
         if (closestFace == null || detectionBox == null) {
             return ""
         }
 
-        if (this.cameraEventListener != null) {
-            if (
-                detectionBox.left < 0 ||
+        val screenWidth = this.graphicView.width
+        val screenHeight = this.graphicView.height
+
+        val isOutOfTheScreen =
+            detectionBox.left < 0 ||
                 detectionBox.top < 0 ||
-                detectionBox.right > this.graphicView.width ||
-                detectionBox.bottom > this.graphicView.height
+                detectionBox.right > screenWidth ||
+                detectionBox.bottom > screenHeight
+
+        if (isOutOfTheScreen) {
+            return ""
+        }
+
+        // This variable is the face detection box percentage in relation with the
+        // UI graphic view. The value must be between 0 and 1.
+        val detectionBoxRelatedWithScreen: Float = detectionBox.width() / screenWidth
+
+        if (detectionBoxRelatedWithScreen < this.captureOptions.faceCaptureMinSize) {
+            return Message.INVALID_CAPTURE_FACE_MIN_SIZE
+        }
+
+        if (detectionBoxRelatedWithScreen > this.captureOptions.faceCaptureMaxSize) {
+            return Message.INVALID_CAPTURE_FACE_MAX_SIZE
+        }
+
+        if (this.captureOptions.faceROI.enable) {
+
+            // Detection box offsets.
+            val topOffset: Float = detectionBox.top / screenHeight
+            val rightOffset: Float = (screenWidth - detectionBox.right) / screenWidth
+            val bottomOffset: Float = (screenHeight - detectionBox.bottom) / screenHeight
+            val leftOffset: Float = detectionBox.left / screenWidth
+
+            if (this.captureOptions.faceROI.isOutOf(
+                    topOffset,
+                    rightOffset,
+                    bottomOffset,
+                    leftOffset
+                )
             ) {
-                return ""
+                return Message.INVALID_CAPTURE_FACE_OUT_OF_ROI
             }
 
-            // This variable is the face detection box percentage in relation with the
-            // UI graphic view. The value must be between 0 and 1.
-            val detectionBoxRelatedWithScreen: Float =
-                detectionBox.width() / this.graphicView.width
+            if (this.captureOptions.faceROI.hasChanges) {
 
-            if (detectionBoxRelatedWithScreen < this.captureOptions.faceCaptureMinSize) {
-                return Message.INVALID_CAPTURE_FACE_MIN_SIZE
-            }
-            if (detectionBoxRelatedWithScreen > this.captureOptions.faceCaptureMaxSize) {
-                return Message.INVALID_CAPTURE_FACE_MAX_SIZE
+                // Face is inside the region of interest and faceROI is setted.
+                // Face is smaller than the defined "minimumSize".
+                val roiWidth: Float =
+                    screenWidth -
+                        ((this.captureOptions.faceROI.rightOffset + this.captureOptions.faceROI.leftOffset) * screenWidth)
+                val faceRelatedWithROI: Float = detectionBox.width() / roiWidth
+
+                if (this.captureOptions.faceROI.minimumSize > faceRelatedWithROI) {
+                    return Message.INVALID_CAPTURE_FACE_ROI_MIN_SIZE
+                }
             }
         }
 
