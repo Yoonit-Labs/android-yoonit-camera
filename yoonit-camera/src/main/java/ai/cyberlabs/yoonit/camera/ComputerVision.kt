99b41e8056ce4c9903631f8ebb07550c5623b3cd
HaroldoTeruya <haroldo.s.teruya@gmail.com>
2021-09-08 09:57:29 -0300

    feat: add computer vision input size

--- yoonit-camera/src/main/java/ai/cyberlabs/yoonit/camera/ComputerVision.kt
+++ yoonit-camera/src/main/java/ai/cyberlabs/yoonit/camera/ComputerVision.kt
@@ -1,18 +1,15 @@
 package ai.cyberlabs.yoonit.camera
 
 import ai.cyberlabs.yoonit.camera.models.CaptureOptions
+import android.util.Size
 import java.io.File
 
 class ComputerVision {
+
     /**
      * Enable/disable computer vision usage.
-     *
-     * @param enable The indicator to enable/disable computer vision usage.
      * Default value is `false`.
      */
-    fun setComputerVision(enable: Boolean) {
-        CaptureOptions.computerVision.enable = enable
-    }
     var enable: Boolean = CaptureOptions.computerVision.enable
         set(value) {
             CaptureOptions.computerVision.enable = value
@@ -21,20 +18,8 @@ class ComputerVision {
 
     /**
      * Set the computer vision model paths to load.
-     *
-     * @param modelPaths The computer vision absolute model file path array list.
      * Default value is an empty array.
      */
-    fun setComputerVisionLoadModels(modelPaths: ArrayList<String>) {
-        modelPaths.forEach {
-                modelPath ->
-            if (!File(modelPath).exists()) {
-                throw IllegalArgumentException("${KeyError.INVALID_COMPUTER_VISION_MODEL_PATHS}: $modelPath")
-            }
-        }
-
-        CaptureOptions.computerVision.paths = modelPaths
-    }
     var modelPaths: ArrayList<String> = CaptureOptions.computerVision.paths
         set(value) {
             value.forEach {
@@ -48,12 +33,15 @@ class ComputerVision {
             field = value
         }
 
+    var inputSize: Size = CaptureOptions.computerVision.inputSize
+        set(value) {
+            CaptureOptions.computerVision.inputSize = value
+            field = value
+        }
+
     /**
      * Clear loaded computer vision models.
      */
-    fun computerVisionClearModels() {
-        CaptureOptions.computerVision.clear()
-    }
     fun clear() {
         CaptureOptions.computerVision.clear()
     }
