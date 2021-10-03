325c547d7276ad1305c4d6e6b950dca560ab8b55
Luigui Delyer <git@s1x.com.br>
2021-10-03 09:42:10 +0200

    Improving header files

--- yoonit-camera/src/main/java/ai/cyberlabs/yoonit/camera/models/ROI.kt
+++ yoonit-camera/src/main/java/ai/cyberlabs/yoonit/camera/models/ROI.kt
@@ -11,7 +11,7 @@
  * Yoonit Camera
  * The most advanced and modern Camera module for Android with a lot of awesome features
  *
- * Haroldo Teruya & Victor Goulart @ 2020-2021
+ * Haroldo Teruya, Victor Goulart, Thúlio Noslen & Luigui Delyer @ 2020-2021
  */
 
 package ai.cyberlabs.yoonit.camera.models
