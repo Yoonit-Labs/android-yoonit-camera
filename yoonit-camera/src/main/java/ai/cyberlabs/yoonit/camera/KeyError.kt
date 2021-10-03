acf53fbbba25d16866d394ab4a0588b0f80dc233
Luigui Delyer <git@s1x.com.br>
2021-10-03 09:31:27 +0200

    improving descriptions

--- yoonit-camera/src/main/java/ai/cyberlabs/yoonit/camera/KeyError.kt
+++ yoonit-camera/src/main/java/ai/cyberlabs/yoonit/camera/KeyError.kt
@@ -1,12 +1,17 @@
 /**
- * +-+-+-+-+-+-+
- * |y|o|o|n|i|t|
- * +-+-+-+-+-+-+
+ * ██╗   ██╗ ██████╗  ██████╗ ███╗   ██╗██╗████████╗
+ * ╚██╗ ██╔╝██╔═══██╗██╔═══██╗████╗  ██║██║╚══██╔══╝
+ *  ╚████╔╝ ██║   ██║██║   ██║██╔██╗ ██║██║   ██║
+ *   ╚██╔╝  ██║   ██║██║   ██║██║╚██╗██║██║   ██║
+ *    ██║   ╚██████╔╝╚██████╔╝██║ ╚████║██║   ██║
+ *    ╚═╝    ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝╚═╝   ╚═╝
  *
- * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
- * | Yoonit Camera lib for Android applications                      |
- * | Haroldo Teruya & Victor Goulart @ Cyberlabs AI 2020-2021        |
- * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * https://yoonit.dev - about@yoonit.dev
+ *
+ * Yoonit Camera
+ * The most advanced and modern Camera module for Android with a lot of awesome features
+ *
+ * Haroldo Teruya & Victor Goulart @ 2020-2021
  */
 
 package ai.cyberlabs.yoonit.camera
